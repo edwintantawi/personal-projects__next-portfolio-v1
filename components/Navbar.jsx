7196f914a0ea6139a1d03bf33bc15221945556ec
edwintantawi <edwintantawi@gmail.com>
2021-05-29 16:03:23 +0700

    adding mobile navigation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 const NAV_LINK = [
@@ -12,29 +13,49 @@ const NAV_LINK = [
 ];
 
 const Navbar = () => {
+  const [isActiveMenu, setIsActiveMenu] = useState(false);
   const route = useRouter().route;
 
+  document
+    .querySelector('body')
+    .addEventListener('click', handleToggleMenuClick);
+
+  const handleToggleMenuClick = (event) => {
+    setIsActiveMenu((prev) => !prev);
+    event.stopPropagation();
+  };
+
   return (
-    <nav className=" mx-auto p-5 fixed w-full bg-white">
+    <nav className=" mx-auto p-4 lg:p-6 fixed w-full bg-white">
       <div className="flex justify-between container-sm sm:container">
         <Link href="/">
           <a className="flex">
             <Image src="/logo.svg" width="44px" height="30px" />
           </a>
         </Link>
-        <ul className="hidden space-x-10 md:flex">
+        <ul
+          className={`${
+            !isActiveMenu && 'transform translate-x-[100vw]'
+          } w-4/5 xs:w-2/3 sm:!w-3/6 md:!w-auto bg-white py-12 px-12 md:py-0 md:px-0 border md:border-none transition duration-500 flex flex-col md:flex-row fixed md:static top-16 bottom-0 right-0 md:space-x-10 md:translate-x-0 space-y-6 md:space-y-0`}>
           {NAV_LINK.map((link, index) => (
-            <li
-              key={index}
-              className={`flex items-center ${
-                route === link.url && 'link-active'
-              }`}>
+            <li key={index} className="flex items-center">
               <Link href={link.url}>
-                <a>{link.title}</a>
+                <a className={`${route === link.url && 'link-active'}`}>
+                  {link.title}
+                </a>
               </Link>
             </li>
           ))}
         </ul>
+        <a
+          href="#"
+          className="grid md:hidden w-8 h-8 place-items-center"
+          onClick={handleToggleMenuClick}>
+          <i
+            className={`fas ${
+              isActiveMenu ? 'fa-times' : 'fa-bars'
+            } text-3xl text-black-900`}></i>
+        </a>
       </div>
     </nav>
   );
