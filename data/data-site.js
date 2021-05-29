282c3b4f5bbab68af9b4a57fb68bf0bd0ba70d63
edwintantawi <edwintantawi@gmail.com>
2021-05-29 19:30:02 +0700

    adding datasite

---
+++ data/data-site.js
@@ -0,0 +1,95 @@
+const dataSite = {
+  naviagtion: [
+    { title: 'Home', url: '/' },
+    { title: 'Resume', url: '/resume' },
+    { title: 'Project', url: '/project' },
+    { title: 'Blogs', url: '/blogs' },
+    { title: 'About Me', url: '/aboutme' },
+    { title: 'Contact Me', url: '/contactme' },
+  ],
+  hero: {
+    name: 'Edwin Tantawi',
+    title: 'Frontend Web Developer',
+    short_bio:
+      'I am a student from Indonesia, who focuses on web development, especially frontend web development.',
+  },
+  what_can_i_do: [
+    {
+      icon: '/uiuxdesign.svg',
+      title: 'Ui Ux design',
+      description:
+        'Develop a good user interface and user experience, with a beautiful and comfortable appearance for the user',
+    },
+    {
+      icon: '/websitedev.svg',
+      title: 'Website Development',
+      description:
+        'Developing a website appearance based on design into a website that can be use. With good performance',
+    },
+    {
+      icon: '/webappsdev.svg',
+      title: 'WebApps Development ',
+      description:
+        'Developing a very well-functioning web applications, various features and comfortable website performance',
+    },
+  ],
+  skills_technology: [
+    {
+      icon: '/techstack/html5.svg',
+      title: 'HTML 5',
+    },
+    {
+      icon: '/techstack/css3.svg',
+      title: 'CSS 3',
+    },
+    {
+      icon: '/techstack/javascript.svg',
+      title: 'Javascript',
+    },
+    {
+      icon: '/techstack/webpack.svg',
+      title: 'webpack 5',
+    },
+    {
+      icon: '/techstack/reactjs.svg',
+      title: 'React JS',
+    },
+    {
+      icon: '/techstack/firebase.svg',
+      title: 'Firebase',
+    },
+    {
+      icon: '/techstack/git.svg',
+      title: 'GIT',
+    },
+    {
+      icon: '/techstack/github.svg',
+      title: 'Github',
+    },
+  ],
+  projects: [
+    {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/edwintantawi-25f09.appspot.com/o/projects%2Fcookingboxs.png?alt=media',
+      title: 'CookingBox',
+      stack: 'HTML, CSS, PWA',
+      link: 'https://cookingboxs.netlify.app',
+    },
+    {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/edwintantawi-25f09.appspot.com/o/projects%2Fnetflixclone.png?alt=media',
+      title: 'Netflix Clone',
+      stack: 'React JS, CSS, TMDB API',
+      link: 'https://clone-netflix-web.netlify.app/',
+    },
+    {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/edwintantawi-25f09.appspot.com/o/projects%2Fwhatsappclone.png?alt=media',
+      title: 'Whatsapp Clone',
+      stack: 'React JS, CSS, Firebase',
+      link: 'https://clone-whatsapp-web.netlify.app/',
+    },
+  ],
+};
+
+export default dataSite;
